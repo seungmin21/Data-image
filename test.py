0e69727aec99e742ba645553c4d41d959c2e4365
ohseungmin <ohmin2886@gmail.com>
2024-01-19 15:58:51 +0900

    폰트깨짐 방지 matplotlib는 기본적으로 한글 지원하지 않기 때문에 폰트 설정 필요

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# 한글 폰트 설정
+font_path = "C:/Windows/Fonts/malgun.ttf"  # 사용하는 운영체제의 한글 폰트 경로로 변경
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
 
 # 데이터 생성
 x = [1, 2, 3, 4, 5]
